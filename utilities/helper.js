6027e39ab42dd90d4504140194a3127fc3ab862c
sumancx31 <sumansah029@gmail.com>
2026-05-05 23:33:24 +0545

    auth model complete

---
+++ utilities/helper.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const randomStringGenerator = (length = 100) => {
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const len = chars.length;
+    let random = ''
+
+    for(let i = 0; i < length; i++){
+        const posn = Math.ceil(Math.random() * len);
+        random += chars[posn];
+    }
+    return random;
+}
+
+const deleteFile = (filePath) => {
+    if(fs.existsSync(filePath)){
+        fs.unlinkSync(filePath);
+    }
+}
+module.exports = {
+    randomStringGenerator,
+    deleteFile,
+}
